ebebbd834c9df7c5fc5b5b3754fb2598727da5de
Tyler Roberts <mcszymist7@gmail.com>
2018-02-07 08:18:11 -0900

    Local CPP changed pushing to cause merge conflict.

--- break.cpp
+++ break.cpp
@@ -1,14 +1,22 @@
-#include <vector>
-using std::vector;
+#include <string>
+using std::string;
+using std::stoi;
 #include <iostream>
 using std::cout;
 using std::endl;
 
 int main(){
     int total=0;
-    vector<int> vecOfInt = {100,5,6,234,6457,1,2345,876,5234,5,57,5,325,455,6};
-    for(auto i : vecOfInt){
-        total += i;
+    string number;
+    string stringOfInt = "100,5,6,234,6457,1,2345,876,5234,5,57,5,325,455,6";
+    for(auto i : stringOfInt){
+        if(i == ','){
+            total += stoi(number);
+            number = std::string();
+        }
+        else{
+            number += i;
+        }
     }
     cout << total << endl;
     return 0;
