956f5442b64d5f8b716e7e08975df525510cf397
mcszymist <35680154+mcszymist@users.noreply.github.com>
2018-02-07 08:07:46 -0900

    Update break.cpp
    "Github Changes Vector to Stack"

--- break.cpp
+++ break.cpp
@@ -1,13 +1,13 @@
-#include <vector>
-using std::vector;
+#include <stack>
+using std::stack;
 #include <iostream>
 using std::cout;
 using std::endl;
 
 int main(){
-    int total=0;
-    vector<int> vecOfInt = {100,5,6,234,6457,1,2345,876,5234,5,57,5,325,455,6};
-    for(auto i : vecOfInt){
+    int total = 0;
+    stack<int> stackOfInt = {100,5,6,234,6457,1,2345,876,5234,5,57,5,325,455,6};
+    for(auto i : stackOfInt){
         total += i;
     }
     cout << total << endl;
